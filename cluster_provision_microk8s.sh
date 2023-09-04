31052cb529344543d6ae79d0216150d450fd1369
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-09-04 19:46:43 +0300

    Change dashboard installation source to helm

--- cluster_provision_microk8s.sh
+++ cluster_provision_microk8s.sh
@@ -10,14 +10,17 @@ echo "alias kubectl='microk8s kubectl'" | sudo tee -a /etc/bash.bashrc
 microk8s enable rbac
 microk8s enable hostpath-storage
 microk8s enable ingress
-microk8s enable dashboard
 microk8s enable metrics-server
 microk8s enable prometheus
-microk8s kubectl create ns kubernetes-dashboard
+microk8s helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+microk8s helm repo update
+microk8s helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+  --namespace=kubernetes-dashboard \
+  --create-namespace
 microk8s kubectl apply -f configs/dashboard-sa.yml
 microk8s kubectl apply -f configs/dashboard-crb.yml
 microk8s kubectl apply -f configs/dashboard-secret.yml
 sudo mkdir -p /home/ubuntu/secrets
 microk8s kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d > /home/ubuntu/secrets/dashboard_token.txt
 sudo chown ubuntu:ubuntu /home/ubuntu/secrets
-sudo chown ubuntu:ubuntu /home/ubuntu/secrets/*
+sudo chown ubuntu:ubuntu /home/ubuntu/secrets/**
