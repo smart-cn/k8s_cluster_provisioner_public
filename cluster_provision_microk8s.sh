0cf8b2788906f276f57f28f8f3c745ad9aa53a45
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-09-05 16:04:22 +0300

    Implement Grafana installation to the cluster

--- cluster_provision_microk8s.sh
+++ cluster_provision_microk8s.sh
@@ -25,5 +25,13 @@ microk8s kubectl apply -f configs/dashboard-crb.yml
 microk8s kubectl apply -f configs/dashboard-secret.yml
 sudo mkdir -p /home/ubuntu/secrets
 microk8s kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d > /home/ubuntu/secrets/dashboard_token.txt
+microk8s helm repo add grafana https://grafana.github.io/helm-charts
+microk8s helm repo update
+microk8s helm install grafana grafana/grafana \
+  --namespace=grafana \
+  --create-namespace \
+  --set env.GF_SERVER_ROOT_URL="http://localhost/grafana/" \
+  --wait
+microk8s kubectl get secret --namespace grafana grafana -o jsonpath="{.data.admin-password}" | base64 -d > /home/ubuntu/secrets/grafana_token.txt
 sudo chown ubuntu:ubuntu /home/ubuntu/secrets
 sudo chown ubuntu:ubuntu /home/ubuntu/secrets/**
